5a47c76277c8d65cde140d9c9a73f892c9da033e
Erik Holmberg <b21eriho@student.his.se>
2022-04-26 15:00:34 +0200

    Add needed fields to mountain class, add json url and add network permission in manifest

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
 
     private ArrayList<Mountain> mountainList;
 
-    private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
     private final String JSON_FILE = "mountains.json";
 
     @Override
--- app/src/main/java/com/example/networking/Mountain.java
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -2,6 +2,10 @@ package com.example.networking;
 
 public class Mountain {
 
+    private String name;
 
-
+    @Override
+    public String toString() {
+        return name;
+    }
 }
